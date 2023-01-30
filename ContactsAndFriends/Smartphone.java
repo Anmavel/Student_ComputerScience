e869ef90c3de544b0787a7b78ce5029a1f0e057a
Aniii <Ana@example.com>
2023-01-30 19:23:20 +0100

    create Smartphone Class & attributes modelNme, brand & contactList

---
+++ ContactsAndFriends/Smartphone.java
@@ -0,0 +1,12 @@
+package ContactsAndFriends;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Smartphone {
+    private String modelName;
+    private String brand;
+
+    List<Contact> contactlist = new ArrayList<>();
+
+}
